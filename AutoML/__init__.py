b66155f1d5b17c6b64db85763670d4838f0afc93
owen <owen.o.driscoll@ifremer.fr>
2023-02-23 14:07:41 +0100

    added class call for regression and classification specifically. Added classifier_selector

--- AutoML/__init__.py
+++ AutoML/__init__.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from .automl import AutomatedML, AutomatedRegression
+from .automl import AutomatedML, AutomatedRegression, AutomatedClassification
 from .regressors import regressor_selector
+from .classifiers import classifier_selector
 from .function_helper import FuncHelper
 from .scalers_transformers import PcaChooser, PolyChooser, SplineChooser, ScalerChooser, TransformerChooser
 
